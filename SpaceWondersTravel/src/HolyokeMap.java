d12f162c8e9e372a242ef5b5e55c75718f21e27c
AJtheRaven <johns39a@mtholyoke.edu>
2020-11-07 18:35:45 -0800

    started working on menu frame

--- SpaceWondersTravel/src/HolyokeMap.java
+++ SpaceWondersTravel/src/HolyokeMap.java
@@ -57,6 +57,19 @@ public class HolyokeMap extends AbstractAnimation implements KeyListener {
     
     public static STATE State = STATE.MENU;
     
+ // all of these variables are used for different screens
+    private Rectangle quitButton = new Rectangle(WINDOW_WIDTH / 2 + 200, 500,
+            150, 50);
+
+    private Rectangle aboutButton = new Rectangle(WINDOW_WIDTH / 2 - 70, 500,
+            150, 50);
+
+    private Rectangle playButton = new Rectangle(WINDOW_WIDTH / 2 - 330, 500,
+            150, 50);
+
+    private Rectangle playAgainButton = new Rectangle(WINDOW_WIDTH / 2 - 330,
+            500, 150, 50);
+    
     /**
      * Constructs a pandemicGame and initializes it to be able to accept key
      * input.
@@ -95,14 +108,100 @@ public class HolyokeMap extends AbstractAnimation implements KeyListener {
      * @param g the graphics context to draw on
      */
     public void paintComponent(Graphics g) {
-        //g.drawImage(img, 0, 0, null);
+        if (State == STATE.MENU) {
+            menuRender(g);
+        }
+
+        // handles about screen
+        else if (State == STATE.ABOUT) {
+            //aboutRender(g);
+        }
+
+        // handles new level screen
+        else if (State == STATE.LEVEL) {
+            //levelRender(g);
+        }
+
+        // handles gameover screen
+        else if (State == STATE.GAMEOVER) {
+            //gameoverRender(g);
+        }
     }
     
     @Override
     protected void nextFrame() {
-        // TODO Auto-generated method stub
+        this.addMouseListener(mouseInput);
         
+        repaint();
     }
+    
+    
+    /**
+     * Creates the game's intro screen.
+     * 
+     * @param g - the graphics context to draw on
+     */
+    public void menuRender(Graphics g) {
+        Font fnt = new Font("arial", Font.BOLD, 60);
+        Font buttonFont = new Font("Times New Roman", Font.BOLD, 20);
+        g.setFont(fnt);
+        g.setColor(Color.white);
+        g.drawString("Welcome To PANDEMIC!", 100, 100);
+        g.setFont(buttonFont);
+
+        // may need to delete this line
+        g.drawString("Produced by the one and only Space Wonders",
+                this.getWidth() / 4, 20);
+        g.drawString(
+                "The year is 2020, and there is a deadly pandemic sweeping the globe.",
+                40, 150);
+        g.drawString(
+                "Your job is to eliminate it with the power of vaccines! Viruses will appear in random places ",
+                40, 180);
+        g.drawString(
+                "around the screen, going at random speeds, and you have to try to inject them with vaccine  ",
+                40, 210);
+        g.drawString(
+                "particles. You get 5 vaccine needles in total, and you can keep going until you have eliminated ",
+                40, 240);
+        g.drawString(
+                "all of the viruses or until they have eliminated you, at which point you will respawn in a random, ",
+                40, 270);
+        g.drawString(
+                "location. Be careful, though! Each virus will split into  ",
+                40, 300);
+
+        g.drawString(
+                "two after it has been injected, until you reach the smallest size. ",
+                40, 330);
+        g.drawString(
+                "When you have successfully eliminated one round of viruses, the game will switch to the next",
+                40, 360);
+        g.drawString(
+                "level. Each level will contain one more virus than the previous one.",
+                40, 390);
+        g.drawString(
+                "You will also have to face stupid people who have forgotten to wear their masks.",
+                40, 420);
+        g.drawString(
+                "They will kill you if you touch them and will also cough deadly germs out in ",
+                40, 450);
+        g.drawString(
+                "all directions at once, so try to inject or avoid them before you get hit!",
+                40, 480);
+
+        // Creates and draws buttons
+        g.drawString("PLAY", playButton.x + 43, playButton.y + 34);
+        g.drawString("ABOUT", aboutButton.x + 40, aboutButton.y + 34);
+        g.drawString("QUIT", quitButton.x + 41, quitButton.y + 34);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.draw(playButton);
+        g2D.draw(aboutButton);
+        g2D.draw(quitButton);
+
+    }
+    
+    
     
     /**
      * Main method.
